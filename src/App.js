b21cb1ebd6637e8eba0c37777432481f069d555f
Matthew Lockdall <mlockdall26@gmail.com>
2019-08-14 19:44:44 -0500

    user edit

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ function App() {
     getHotList()
     if (localStorage.getItem("user") !== null) {
       console.log('hi');
-       // setUser(JSON.parse(localStorage.getItem("user")))
+       setUser(JSON.parse(localStorage.getItem("user")))
     }
     return () => {
       console.log('clear hot list');
